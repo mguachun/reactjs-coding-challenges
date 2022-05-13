48e65e40078afa92b50853ff4958185c4a8cf2aa
Melissa Guachun <1212malka@gmail.com>
2022-05-13 13:40:27 -0400

    increment/decrement hooks, useState

---
+++ src/CounterTwo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+
+function CounterTwo () {
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    return(
+        <div>
+            <h1>Counter: {count}</h1>
+      
+
+      <button onClick={increment}>Increment </button>
+      <button onClick={decrement}>Decrement </button>
+        </div>
+    )
+}
+
+export default CounterTwo;
