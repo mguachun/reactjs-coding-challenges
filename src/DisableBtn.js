bff6145d879a6d1eebc71df47012a70617a7c3c4
Melissa Guachun <1212malka@gmail.com>
2022-05-16 12:52:55 -0400

    creation of disable btn component

---
+++ src/DisableBtn.js
@@ -0,0 +1,13 @@
+import React from "react";
+function DisableBtn() {
+    const [] = React.useState("");
+  
+    return (
+      <>
+        <h3>Disable Button Challenge</h3>
+        <input type="text" />
+        <button>Submit</button>
+      </>
+    );
+  }
+  export default DisableBtn
