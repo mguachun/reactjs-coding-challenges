91fc5bdee957dd2db150aa7fb6c19109d5037e6b
Melissa Guachun <1212malka@gmail.com>
2022-05-16 13:04:10 -0400

    changed to div

--- src/DisableBtn.js
+++ src/DisableBtn.js
@@ -3,11 +3,11 @@ function DisableBtn() {
     const [value, setValue] = React.useState("");
   
     return (
-      <>
+      <div>
         <h3>Disable Button Challenge</h3>
         <input type="text" onChange={(e) => setValue(e.target.value)}/>
         <button disabled={value.length < 1}>Submit</button>
-      </>
+      </div>
     );
   }
   export default DisableBtn
